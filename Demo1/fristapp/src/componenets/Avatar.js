f4b2e7f2a817b18378700b2339d0b9baf8d3a7d2
ibrahimMR <ibrahim.mahmoud@sed.svu.edu.eg>
2023-09-22 14:13:50 +0300

    edit Avatar Module file

--- Demo1/fristapp/src/componenets/Avatar.js
+++ Demo1/fristapp/src/componenets/Avatar.js
@@ -1,6 +1,7 @@
 import "../App.css"
 
 function Avatar(props) {
+	//use url from props where saved in App file >> as const >> user
 	return <img className='logo' src={props.imgurl} alt={props.name} />
 }
 
